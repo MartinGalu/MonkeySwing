db98d68cc2223816f54d23e4ddbc76df8761145c
Martin Galusinski <martin.galusinski@dhl.com>
2022-05-19 09:03:54 +0200

    Add the dashboard component

--- Services/MonkeySwing/src/app/app.module.ts
+++ Services/MonkeySwing/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppComponent } from './app.component';
 import { MatSliderModule } from "@angular/material/slider";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
